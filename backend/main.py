e6d356692107a23e7d502978500655196a4de3b4
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2021-11-30 18:47:02 +0100

    add CORS policy

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+	https://spages.mini.pw.edu.pl/~sawickib/stroke-prediction/
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/stroke_proba")
 def predict_proba():
